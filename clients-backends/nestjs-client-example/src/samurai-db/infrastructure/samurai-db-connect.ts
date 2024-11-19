9fcbf79eff5162af8c84272fb9a524e21c108fbf
Музыка Владислав <ru9art@gmail.com>
2024-11-19 16:07:28 +0300

    feat(fix): Fix reconnect to db

--- clients-backends/nestjs-client-example/src/samurai-db/infrastructure/samurai-db-connect.ts
+++ clients-backends/nestjs-client-example/src/samurai-db/infrastructure/samurai-db-connect.ts
@@ -10,24 +10,24 @@ export class SamuraiDBConnect extends ConnectionService {
   private retryInterval: number;
   private attempt: number = 0; // Инициализация попыток с 0
   protected status: 'CONNECTING' | 'CONNECTED' = 'CONNECTING';
-  private onRejectHandler: () => void =  () => {}
-
+  private onRejectHandler: () => void = () => {};
+  private onConnectHandler: () => void = () => {};
 
   constructor(@Inject(MODULE_OPTIONS_TOKEN) private options: ModuleOptions) {
-    super()
+    super();
     this.retryInterval = options.initialRetryInterval;
     this.connect();
   }
 
   private reconnect(err) {
-    this.onRejectHandler()
+    this.onRejectHandler();
     console.error('Connection error:', err?.message);
     this.status = 'CONNECTING';
     this.attempt++; // Увеличение счетчика попыток
 
     if (this.attempt <= this.options.maxRetries) {
       console.log(
-          `Attempt ${this.attempt} failed. Retrying in ${this.retryInterval / 1000}s...`,
+        `Attempt ${this.attempt} failed. Retrying in ${this.retryInterval / 1000}s...`,
       );
       setTimeout(() => this.connect(), this.retryInterval);
       this.retryInterval *= 2; // Увеличение времени задержки
@@ -44,21 +44,26 @@ export class SamuraiDBConnect extends ConnectionService {
         this.status = 'CONNECTED';
         this.retryInterval = this.options.initialRetryInterval; // Сброс интервала при успешном подключении
         this.attempt = 0; // Сброс счетчика попыток при успешном подключении
+
+        this.onConnectHandler();
       },
     );
 
     this.client.on('error', (err) => {
-      console.log('on error')
-        this.reconnect(err)
+      console.log('on error');
+      this.reconnect(err);
     });
 
     this.client.on('end', (err) => {
-      console.log('on end')
-      this.reconnect(err)
+      console.log('on end');
+      this.reconnect(err);
     });
   }
 
   public onReject(callback: () => void) {
     this.onRejectHandler = callback;
+  }
+  public onConnect(callback: () => void) {
+    this.onConnectHandler = callback;
   }
 }
